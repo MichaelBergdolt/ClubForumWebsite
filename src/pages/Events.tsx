065c18eb3a3438a786d7a832a65b466101a7a550
Michael Bergdolt <michaelbergdolt20@gmail.com>
2025-08-30 14:06:46 +0200

    hintergrund wieder weiß

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -293,8 +293,7 @@ const Events = () => {
       )}
 
       {/* Kommende Events */}
-
-      <section className="py-20 bg-gradient-to-b from-gray-950 to-black">
+      <section className="py-20">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-4xl font-bold text-center mb-16">
             Weitere Highlights
