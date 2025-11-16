7f5c23f00c6581ab1647b273b6fdb9a7eb0593fb
Michael Bergdolt <michaelbergdolt20@gmail.com>
2025-11-16 19:34:48 +0100

    added .htaccess for for SPA Routing

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,4 @@
+<!-- TODO: in Response dürfen auf keinen Fall Kalenderdaten enthalten sein! Datenschutz!-->
 <?php
 // === CORS-Setup ===
 ini_set('display_errors', 1); # TODO: darf auf keinen Fall in Production build sein! Unsicher!
