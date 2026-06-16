2c7e7fd74fc3760e4566b332ec080a5ebe38f770
MichaelBergdolt <michaelbergdolt20@gmail.com>
2026-06-16 11:46:31 +0200

    updated summer vibes

--- frontend/src/data/events.ts
+++ frontend/src/data/events.ts
@@ -165,8 +165,8 @@ export const upcomingEvents: UpcomingEvent[] = [
     date: "01.08.2026",
     locationName: "Club Forum",
     locationAddress: "Marktstraße 9, 71032 Böblingen",
-    description: "Das Event, um den Sommer in vollen Zügen zu genießen. Freut euch auf entspannte Beats, kalte Getränke und eine unvergessliche Atmosphäre mitten im Herzen von Böblingen. Kommt vorbei, bringt eure Crew mit und lasst uns gemeinsam bei guten Vibes den perfekten Sommertag feiern!",
-    image: "/images/events/past_events/stadtfest_2025/Stadtfest_4.jpg"
+    description: "Wir laden euch herzlich zu unserem Sommerfest ein. Schnappt eure Sonnenbrille, schaltet vom Alltag ab und genießt das lockere Open-Air-Ambiente bei kühlen Drinks und entspannter Musik. Ein perfekter Tag, um einfach mal die Seele baumeln zu lassen und mit alten und neuen Gesichtern zu quatschen.",
+    image: "/images/events/past_events/stadtfest_2025/Stadtfest_2.jpg"
   },
   {
     title: "Halloween",
